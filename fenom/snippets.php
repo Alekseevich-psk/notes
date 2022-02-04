5148331d55ff422357bf062a14b479aafcfc3331
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2022-02-04 22:52:26 +0300

    Update snippets.php

--- fenom/snippets.php
+++ fenom/snippets.php
@@ -1,5 +1,9 @@
 <?php
 
+// regPhone
 $p1 = array(" ", "-", "(", ")");
 $p2 = array("", "", "", "");
 return str_replace($p1, $p2, $input);
+
+// {set $phone = $_modx->config.phone}
+// <a class="phone" href="tel:{$_modx->runSnippet('regPhone', ['input' => $phone])}">{$phone}</a>
